aa4ceb25ef713e9474597b04745e4fc9dcd3fe79
diazbumma <odiazbumma@gmail.com>
2020-11-10 09:37:08 +0700

    Two way binding, and text change handler

--- src/components/NewItem/NewItem.js
+++ src/components/NewItem/NewItem.js
@@ -1,20 +1,31 @@
+import {useState} from 'react'
+
 import './NewItem.css'
 
 const NewItem = (props) => {
+    let [inputText, setInputText] = useState('') 
+
     const addItemHandler = (event) => {
         event.preventDefault();
 
         const newItem = {
             id: 'mvp' + (Math.floor((Math.random() * 100) + 1)),
-            text: 'Lakad Matataaaaaag'
+            text: inputText
         }
+
+        //console.log(inputText)
+        setInputText = ''
 
         props.addNewItem(newItem)
     }
 
+    const textChangeHandler = (event) => {
+        setInputText(event.target.value)
+    }
+
     return (       
         <form className="item-form" onSubmit={addItemHandler}>
-            <input type="text" />
+            <input type="text" value={inputText} onChange={textChangeHandler} />
             <button type="submit">Add Movie</button>
         </form>
     );
